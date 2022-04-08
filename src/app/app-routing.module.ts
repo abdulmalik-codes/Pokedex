bfa61d86d1913ea8a9fe88390e3b0fda7fabad11
abdulmalik-codes <abdul.malik.codes@gmail.com>
2022-04-08 07:49:31 +0200

    deleted pokedex route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/pokedex', pathMatch: 'full' },
-  { path: 'pokedex', component: AppComponent },
-];
+const routes: Routes = [{ path: '', redirectTo: '/', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
